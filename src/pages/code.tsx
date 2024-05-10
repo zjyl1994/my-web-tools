79ff9cb4fb0095d692578c2164ad85ecb6d5fd44
zjyl1994 <me@zjyl1994.com>
2024-05-10 17:40:33 +0800

    add magnify code

--- src/pages/code.tsx
+++ src/pages/code.tsx
@@ -1,5 +1,8 @@
 import { useState, ChangeEvent } from 'react';
+
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 import md5 from 'blueimp-md5';
 import { sha1 } from 'js-sha1';
 
@@ -16,6 +19,11 @@ const Render: React.FC = () => {
     let value = md5(calcParams.mainPassword + calcParams.rememberName + sha1(calcParams.mainPassword + calcParams.rememberName)).substring(0, 16);
     return value.substring(0, 4).toUpperCase() + value.substring(4, 8) + value.substring(8, 12).toUpperCase() + value.substring(12, 16);;
   };
+  const addSeparator = (str: string, separator: string, n: number): string => {
+    const regex = new RegExp(`.{1,${n}}`, 'g');
+    return str.match(regex)?.join(separator) || str;
+  }
+  const doCopy = () => navigator.clipboard.writeText(calcCode()).catch(err => console.error(err));
 
   return (
     <>
@@ -31,6 +39,8 @@ const Render: React.FC = () => {
         <Form.Label>最终密码</Form.Label>
         <Form.Control type="text" value={calcCode()} readOnly />
       </Form.Group>
+      <p className="font-monospace fs-2">{addSeparator(calcCode(),'-',4)}</p>
+      <Button variant="outline-primary" onClick={doCopy}>复制</Button>
     </>
   )
 }
