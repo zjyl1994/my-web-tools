9916474d8993c92c73b6c6ff303fb22826b13583
zjyl1994 <me@zjyl1994.com>
2024-05-10 16:44:18 +0800

    change use single state

--- src/pages/kcal.tsx
+++ src/pages/kcal.tsx
@@ -1,36 +1,38 @@
-import { useState } from 'react';
+import { useState, ChangeEvent } from 'react';
 import Form from 'react-bootstrap/Form';
 
 const Render: React.FC = () => {
-  const [partEngery, setPartEngery] = useState(0);
-  const [partWeight, setPartWeight] = useState(100);
-  const [totalWeight, setTotalWeight] = useState(100);
+    const [calcParams, setCalcParams] = useState({
+        totalWeight: 100,
+        partWeight: 100,
+        partEngery: 0,
+    });
 
-  const handleChange = (setState: React.Dispatch<React.SetStateAction<number>>) =>
-    (event: React.ChangeEvent<HTMLInputElement>) => setState(Number(event.target.value));
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) =>
+        setCalcParams(preValue => { return { ...preValue, [event.target.name]: event.target.value } });
 
-  const totalEngery = (): number => Math.round(totalWeight / partWeight * partEngery * 0.239006);
+    const totalEngery = (): number => Math.round(calcParams.totalWeight / calcParams.partWeight * calcParams.partEngery * 0.239006);
 
-  return (
-    <>
-      <Form.Group className="mb-3">
-        <Form.Label>每份能量(千焦)</Form.Label>
-        <Form.Control type="number" value={partEngery} onChange={handleChange(setPartEngery)} />
-      </Form.Group>
-      <Form.Group className="mb-3">
-        <Form.Label>每份重量</Form.Label>
-        <Form.Control type="number" value={partWeight} onChange={handleChange(setPartWeight)} />
-      </Form.Group>
-      <Form.Group className="mb-3">
-        <Form.Label>总重量</Form.Label>
-        <Form.Control type="number" value={totalWeight} onChange={handleChange(setTotalWeight)} />
-      </Form.Group>
-      <Form.Group className="mb-3">
-        <Form.Label>总能量(大卡)</Form.Label>
-        <Form.Control type="number" value={totalEngery()} readOnly />
-      </Form.Group>
-    </>
-  )
+    return (
+        <>
+            <Form.Group className="mb-3">
+                <Form.Label>每份能量(千焦)</Form.Label>
+                <Form.Control type="number" value={calcParams.partEngery} onChange={handleChange} name="partEngery" />
+            </Form.Group>
+            <Form.Group className="mb-3">
+                <Form.Label>每份重量</Form.Label>
+                <Form.Control type="number" value={calcParams.partWeight} onChange={handleChange} name="partWeight" />
+            </Form.Group>
+            <Form.Group className="mb-3">
+                <Form.Label>总重量</Form.Label>
+                <Form.Control type="number" value={calcParams.totalWeight} onChange={handleChange} name="totalWeight" />
+            </Form.Group>
+            <Form.Group className="mb-3">
+                <Form.Label>总能量(大卡)</Form.Label>
+                <Form.Control type="number" value={totalEngery()} readOnly />
+            </Form.Group>
+        </>
+    )
 }
 
 export default Render
