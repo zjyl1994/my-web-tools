84212b21ed9e5a66fb8e45983d75c87d34c14616
zjyl1994 <me@zjyl1994.com>
2024-05-14 00:39:14 +0800

    add sql formatter

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { Routes, Route, Link,useMatch  } from "react-router-dom";
+import { Routes, Route, Link, useMatch } from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
-import FrontPage from '@/pages/frontpage'
-import JsonPage from '@/pages/json'
-import EncoderPage from '@/pages/encoder'
-import KcalCalcPage from '@/pages/kcal'
-import CodeCalcPage from '@/pages/code'
+import FrontPage from '@/pages/frontpage';
+import JsonPage from '@/pages/json';
+import EncoderPage from '@/pages/encoder';
+import KcalCalcPage from '@/pages/kcal';
+import CodeCalcPage from '@/pages/code';
+import SQLFmtPage from '@/pages/sqlfmt';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -16,29 +17,31 @@ import 'react-toastify/dist/ReactToastify.css';
 function App() {
   return (
     <>
-      <Navbar expand="lg" bg="dark" data-bs-theme="dark" sticky="top">
+      <Navbar expand="lg" bg="dark" data-bs-theme="dark" sticky="top" collapseOnSelect>
         <Container>
           <Navbar.Brand as={Link} to="/">鱼 sifu 工具包</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link as={Link} to="/json" active={Boolean(useMatch('/json'))}>JSON</Nav.Link>
-              <Nav.Link as={Link} to="/encoder" active={Boolean(useMatch('/encoder'))}>编解码</Nav.Link>
-              <Nav.Link as={Link} to="/kcal" active={Boolean(useMatch('/kcal'))}>大卡计算</Nav.Link>
+              <Nav.Link as={Link} to="/json" active={Boolean(useMatch('/json'))}  eventKey="1">JSON</Nav.Link>
+              <Nav.Link as={Link} to="/encoder" active={Boolean(useMatch('/encoder'))}  eventKey="2">编解码</Nav.Link>
+              <Nav.Link as={Link} to="/kcal" active={Boolean(useMatch('/kcal'))}  eventKey="3">大卡计算</Nav.Link>
               <NavDropdown title="其他" id="basic-nav-dropdown">
-                <NavDropdown.Item as={Link} to="/code" active={Boolean(useMatch('/code'))}>密码机</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/code" active={Boolean(useMatch('/code'))}  eventKey="4">密码机</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/sqlfmt" active={Boolean(useMatch('/sqlfmt'))}  eventKey="5">SQL 格式化</NavDropdown.Item>
               </NavDropdown>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      <Container className="mt-3">
+      <Container className="my-3">
         <Routes>
           <Route path="/" element={<FrontPage />} />
           <Route path="/json" element={<JsonPage />} />
           <Route path="/encoder" element={<EncoderPage />} />
           <Route path="/kcal" element={<KcalCalcPage />} />
           <Route path="/code" element={<CodeCalcPage />} />
+          <Route path="/sqlfmt" element={<SQLFmtPage />} />
         </Routes>
       </Container>
       <ToastContainer />
@@ -46,4 +49,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
---
+++ src/pages/sqlfmt/index.tsx
@@ -0,0 +1,31 @@
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
+
+import { useBasic } from '@/hooks/use-basic';
+import { format } from 'sql-formatter';
+
+
+const SQLPage: React.FC = () => {
+    const { value, setValue, action, copy, paste } = useBasic('');
+
+    return (
+        <>
+            <Form.Control as="textarea" rows={20} spellCheck={false} value={value} onChange={e => setValue(e.target.value)} />
+
+            <ButtonToolbar className="mt-2">
+                <ButtonGroup className="me-2">
+                    <Button variant="outline-primary" onClick={action(text => format(text, { language: 'mysql' }))}>格式化</Button>
+                </ButtonGroup>
+                <ButtonGroup className="me-2">
+                    <Button variant="outline-primary" onClick={copy}>复制</Button>
+                    <Button variant="outline-primary" onClick={paste}>粘贴</Button>
+                    <Button variant="outline-primary" onClick={() => setValue('')}>清空</Button>
+                </ButtonGroup>
+            </ButtonToolbar>
+        </>
+    )
+}
+
+export default SQLPage
