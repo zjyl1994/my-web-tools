3b2f7316d05adfee9f6b560899a31c37c68af917
zjyl1994 <me@zjyl1994.com>
2024-05-21 11:34:33 +0800

    split chunk

--- vite.config.ts
+++ vite.config.ts
@@ -5,33 +5,50 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': '/src',
+    resolve: {
+        alias: {
+            '@': '/src',
+        },
     },
-  },
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: '鱼 sifu 工具包',
-        short_name: '工具包',
-        description: '鱼 sifu 的在线小工具合集包',
-        theme_color: 'rgb(248, 249, 250)',
-        icons: [
-          {
-            src: 'android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    }),
-  ],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                name: '鱼 sifu 工具包',
+                short_name: '工具包',
+                description: '鱼 sifu 的在线小工具合集包',
+                theme_color: 'rgb(248, 249, 250)',
+                icons: [
+                    {
+                        src: 'android-chrome-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'android-chrome-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    }
+                ]
+            }
+        }),
+    ],
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id: string) {
+                    if (id.includes('node_modules')) {
+                        if (id.includes('sql-formatter')) {
+                            return 'vendor-sqlfmt';
+                        } else if (id.includes('react')) {
+                            return 'vendor-react';
+                        } else {
+                            return 'vendor-others';
+                        }
+                    }
+                }
+            }
+        }
+    }
 })
