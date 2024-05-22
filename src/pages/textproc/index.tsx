3f18390999911d406eac1d00917143489e23d34d
zjyl1994 <me@zjyl1994.com>
2024-05-22 10:03:17 +0800

    update ui

--- src/pages/textproc/index.tsx
+++ src/pages/textproc/index.tsx
@@ -30,7 +30,7 @@ const TextProcPage: React.FC = () => {
 
             <InputGroup className="mt-2">
                 <InputGroup.Text>正则表达式</InputGroup.Text>
-                <Form.Control onChange={e => setRegexValue(e.target.value)} />
+                <Form.Control onChange={e => setRegexValue(e.target.value)} spellCheck={false}/>
                 <Button variant="outline-primary" onClick={action(regex_filter_lines(regexValue, false))} title="保留符合正则的行">包含</Button>
                 <Button variant="outline-primary" onClick={action(regex_filter_lines(regexValue, true))} title="删除符合正则的行">排除</Button>
                 <Button variant="outline-primary" onClick={action(regex_extract_lines(regexValue))} title="提取正则匹配到的组为 Excel 文本">提取</Button>
@@ -45,38 +45,36 @@ const TextProcPage: React.FC = () => {
                 <span className="me-2">平均行长度 {nums_average(valueLinesLength).toFixed(2)}</span>
             </div>
 
-            <ButtonToolbar className="mt-2">
-                <ButtonGroup className="me-2">
+            <ButtonToolbar>
+                <ButtonGroup className="me-2 mt-2">
                     <Button variant="outline-primary" onClick={action(split_by_comma)}>逗号切行</Button>
                     <Button variant="outline-primary" onClick={action(join_with_comma)}>逗号合行</Button>
                 </ButtonGroup>
-                <ButtonGroup className="me-2">
+                <ButtonGroup className="me-2 mt-2">
                     <Button variant="outline-primary" onClick={action(remove_quote)}>去引号</Button>
                     <Button variant="outline-primary" onClick={action(add_quote)}>加引号</Button>
                 </ButtonGroup>
-                <ButtonGroup className="me-2">
+                <ButtonGroup className="me-2 mt-2">
                     <Button variant="outline-primary" onClick={action(add_comma_suffix)}>行尾加逗号</Button>
                     <Button variant="outline-primary" onClick={action(remove_comma_suffix)}>行尾去逗号</Button>
                 </ButtonGroup>
-                <ButtonGroup className="me-2">
+                <ButtonGroup className="me-2 mt-2">
                     <Button variant="outline-primary" onClick={action(split_by_blank)}>空白切行</Button>
                     <Button variant="outline-primary" onClick={action(trim_line_space)}>去除两边空白</Button>
                 </ButtonGroup>
-                <ButtonGroup className="me-2">
+                <ButtonGroup className="me-2 mt-2">
                     <Button variant="outline-primary" onClick={action(remove_empty_line)}>去除空行</Button>
                     <Button variant="outline-primary" onClick={action(uniq_line)}>去除重复行</Button>
                 </ButtonGroup>
-            </ButtonToolbar>
-            <ButtonToolbar className="mt-2">
-                <ButtonGroup className="me-2">
+                <ButtonGroup className="me-2 mt-2">
                     <Button variant="outline-primary" onClick={action(sort_asc)}>字典升序排序</Button>
                     <Button variant="outline-primary" onClick={action(sort_desc)}>字典降序排序</Button>
                 </ButtonGroup>
-                <ButtonGroup className="me-2">
+                <ButtonGroup className="me-2 mt-2">
                     <Button variant="outline-primary" onClick={action(sort_len_asc)}>长度升序排序</Button>
                     <Button variant="outline-primary" onClick={action(sort_len_desc)}>长度降序排序</Button>
                 </ButtonGroup>
-                <ButtonGroup className="me-2">
+                <ButtonGroup className="me-2 mt-2">
                     <Button variant="outline-primary" onClick={copy}>复制</Button>
                     <Button variant="outline-primary" onClick={paste}>粘贴</Button>
                     <Button variant="outline-primary" onClick={() => setValue('')}>清空</Button>
