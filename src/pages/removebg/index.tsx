d36f4451f620005bd453843c714bafd054a2f786
zjyl1994 <me@zjyl1994.com>
2025-04-30 14:35:22 +0800

    feat: change ui

--- src/pages/removebg/index.tsx
+++ src/pages/removebg/index.tsx
@@ -1,16 +1,11 @@
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
-import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
-import React, { useRef, useState } from 'react';
-import { performFloodFill, performLegncy } from './utils';
+import React, { useRef } from 'react';
 
 const RemoveBgPage: React.FC = () => {
     const canvasRef = useRef<HTMLCanvasElement>(null);
-    const [backgroundColor, setBackgroundColor] = useState<string>('#ffffff');
-    const [algorithm, setAlgorithm] = useState<string>('Flood Fill算法'); // 新增算法状态
-
+   
     const handleOpenImage = () => {
         const input = document.createElement('input');
         input.type = 'file';
@@ -31,14 +26,6 @@ const RemoveBgPage: React.FC = () => {
 
                                 // 再绘制图片
                                 ctx.drawImage(img, 0, 0);
-
-                                // 获取 (0,0) 坐标点的颜色
-                                const pixelData = ctx.getImageData(0, 0, 1, 1).data;
-                                const r = pixelData[0];
-                                const g = pixelData[1];
-                                const b = pixelData[2];
-                                const hexColor = rgbToHex(r, g, b);
-                                setBackgroundColor(hexColor);
                             }
                         }
                     };
@@ -50,56 +37,16 @@ const RemoveBgPage: React.FC = () => {
         input.click();
     };
 
-    const rgbToHex = (r: number, g: number, b: number): string => {
-        return `#${((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1).toUpperCase()}`;
-    };
-
-    const handleSelectBackground = () => {
-        const input = document.createElement('input');
-        input.type = 'color';
-        input.value = backgroundColor;
-        input.onchange = (event: any) => {
-            setBackgroundColor(event.target.value);
-        };
-        input.click();
-    };
-
     const handleRemoveBackground = () => {
         const canvas = canvasRef.current;
         if (canvas) {
             const ctx = canvas.getContext('2d');
             if (ctx) {
                 const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-                const [targetR, targetG, targetB] = hexToRgb(backgroundColor);
-
-                if (algorithm === 'flood_fill') {
-                    // 从四个角开始 Flood Fill
-                    const corners = [
-                        [0, 0],  // 左上角
-                        [canvas.width - 1, 0],  // 右上角
-                        [0, canvas.height - 1],  // 左下角
-                        [canvas.width - 1, canvas.height - 1]  // 右下角
-                    ];
-
-                    corners.forEach(([x, y]) => {
-                        performFloodFill(imageData, canvas.width, canvas.height, targetR, targetG, targetB, 50, x, y);
-                    });
-                } else {
-                    // 使用Legncy算法处理
-                    performLegncy(imageData, canvas.width, canvas.height, targetR, targetG, targetB, 50);
-                }
 
                 ctx.putImageData(imageData, 0, 0);
             }
         }
-    };
-
-    const hexToRgb = (hex: string): [number, number, number] => {
-        const bigint = parseInt(hex.slice(1), 16);
-        const r = (bigint >> 16) & 255;
-        const g = (bigint >> 8) & 255;
-        const b = bigint & 255;
-        return [r, g, b];
     };
 
     const handleSaveImage = () => {
@@ -125,22 +72,8 @@ const RemoveBgPage: React.FC = () => {
             <ButtonToolbar>
                 <ButtonGroup className="me-2 mt-2">
                     <Button variant="light" className="border" onClick={handleOpenImage}>打开图片</Button>
-                    <Button variant="light" className="border" onClick={handleSelectBackground}>指定底色</Button>
                     <Button variant="light" className="border" onClick={handleRemoveBackground}>去除底色</Button>
                     <Button variant="light" className="border" onClick={handleSaveImage}>保存图片</Button>
-
-                </ButtonGroup>
-                <ButtonGroup className="mt-2">
-                    <DropdownButton
-                        as={ButtonGroup}
-                        variant="light"
-                        className="border"
-                        title={algorithm}
-                        onSelect={(eventKey) => setAlgorithm(eventKey || 'flood_fill')}
-                    >
-                        <Dropdown.Item eventKey="flood_fill">Flood Fill 算法</Dropdown.Item>
-                        <Dropdown.Item eventKey="legncy">Legncy 算法</Dropdown.Item>
-                    </DropdownButton>
                 </ButtonGroup>
             </ButtonToolbar>
         </>
